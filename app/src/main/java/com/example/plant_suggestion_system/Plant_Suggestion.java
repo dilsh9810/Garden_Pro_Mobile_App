2ba112df9382b1aef099a4e314f7d1214b6ce67b
dilsh9810 <dilshfruz333@gmail.com>
2021-12-01 15:30:34 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/plant_suggestion_system/Plant_Suggestion.java
@@ -0,0 +1,14 @@
+package com.example.plant_suggestion_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Plant_Suggestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
